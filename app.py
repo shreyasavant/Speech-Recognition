ea83335c27b3766099ed359f71f3959a7db6ea1a
shreyasavant <shreyasavant10@gmail.com>
2021-05-16 09:46:46 +0530

    test

--- app.py
+++ app.py
@@ -8,5 +8,5 @@ def index():
     return "hello world"
 
 
-if __name__ == "__main__"git push - u origin master:
-    app.run(debug=True, threaded=True)
+if __name__ == "__main__"
+app.run(debug=True, threaded=True)
