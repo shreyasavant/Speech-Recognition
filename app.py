0356559d10d479b1304893536a565af35815b7e0
shreyasavant <shreyasavant10@gmail.com>
2021-05-16 18:42:08 +0530

    Python and google speeh recognition API applied

--- app.py
+++ app.py
@@ -1,11 +1,31 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import speech_recognition as sr
+from werkzeug.utils import redirect
 
 app = Flask(__name__)
 
 
-@app.route("/", methods=["GET"])
+@app.route("/", methods=["GET", "POST"])
 def index():
-    return render_template('index.html')
+    transcript = ""
+    if request.method == "POST":
+        print("FORM DATA RECIEVED")
+
+        if "file" not in request.files:
+            return redirect(request.url)
+
+        file = request.files["file"]
+        if file.filename == "":
+            return redirect(request.url)
+
+        if file:
+            recognizer = sr.Recognizer()
+            audioFile = sr.AudioFile(file)
+            with audioFile as source:
+                data = recognizer.record(source)
+            transcript = recognizer.recognize_google(data, key=None)
+
+    return render_template('index.html', transcript=transcript)
 
 
 if __name__ == "__main__":
