2f3cef18cf2b381c586ab82114ee5cbbe41adaf3
shreyasavant <shreyasavant10@gmail.com>
2021-05-16 09:31:43 +0530

    test

--- app.py
+++ app.py
@@ -2,9 +2,11 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route("/",methods=["GET"])
+
+@app.route("/", methods=["GET"])
 def index():
     return "hello world"
 
-if __name__== "__main__":
+
+if __name__ == "__main__"git push - u origin master:
     app.run(debug=True, threaded=True)
